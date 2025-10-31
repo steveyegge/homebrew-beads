fff7ae185b516779948431ee0de6c68a270e130f
Steve Yegge <stevey@sourcegraph.com>
2025-10-31 01:55:15 -0700

    Update bd formula to v0.20.1

--- Formula/bd.rb
+++ Formula/bd.rb
@@ -1,8 +1,8 @@
 class Bd < Formula
   desc "AI-supervised issue tracker for coding workflows"
   homepage "https://github.com/steveyegge/beads"
-  url "https://github.com/steveyegge/beads/archive/refs/tags/v0.20.0.tar.gz"
-  sha256 "3724bc531ffa185192dbd1ac1debe459647351712edd883880a7d03f7a7bfe0f"
+  url "https://github.com/steveyegge/beads/archive/refs/tags/v0.20.1.tar.gz"
+  sha256 "02cd29b82cd8c0005b0d131eb114549e4848bbec1a197c7e1b9f405a51c2ca23"
   license "MIT"
   head "https://github.com/steveyegge/beads.git", branch: "main"
 
